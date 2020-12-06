eca4362473c91a4a535324af5c465d9f1a050303
sashanikitin <supersashanikitin@gmail.com>
2020-12-06 16:15:38 +0200

    Add TextToSpeech and voice recognition

--- app/src/main/java/sashanikitin/voca/NewStory.java
+++ app/src/main/java/sashanikitin/voca/NewStory.java
@@ -1,9 +1,18 @@
 package sashanikitin.voca;
 
+import android.Manifest;
 import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,18 +21,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
 import sashanikitin.voca.db.Story;
 import sashanikitin.voca.db.Word;
 
-public class NewStory extends AppCompatActivity implements View.OnClickListener {
+public class NewStory extends AppCompatActivity {
 
     public static int[] colors = new int[]{Color.parseColor("#43b020"),
-        Color.parseColor("#FF6700")};
+            Color.parseColor("#FF6700")};
 
     LinearLayout linLayout;
     LayoutInflater ltInflater;
+    EditText editText;
+    public static final Integer RecordAudioRequestCode = 1;
+    private SpeechRecognizer speechRecognizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +59,10 @@ public class NewStory extends AppCompatActivity implements View.OnClickListener 
             }
         });
 
+        Button speechButton = findViewById(R.id.speech);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            checkPermission();
+        }
         Button save = findViewById(R.id.save);
         save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,15 +72,87 @@ public class NewStory extends AppCompatActivity implements View.OnClickListener 
         });
         ltInflater = getLayoutInflater();
         addItem();
+        editText = findViewById(R.id.editTextTextMultiLine);
 
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+                Log.i("SAS", "onReadyForSpeech");
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                Log.i("SAS", "onBeginningOfSpeech");
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+                Log.i("SAS", "onBufferReceived");
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+                Log.i("SAS", "onEndOfSpeech");
+            }
+
+            @Override
+            public void onError(int i) {
+                Log.i("SAS", "onError");
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                String text;
+                Toast.makeText(getApplicationContext(), "Finish", Toast.LENGTH_SHORT).show();
+                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if (editText.getText().toString().isEmpty()) {
+                    text = editText.getText().toString() + data.get(0);
+                } else {
+                    text = editText.getText().toString() + "\n" + data.get(0);
+                }
+                editText.setText(text);
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+                Log.i("SAS", "onPartialResults");
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+                Log.i("SAS", "onEvent");
+            }
+        });
+
+        speechButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startListen();
+            }
+        });
+    }
+
+
+    protected void startListen() {
+        Toast.makeText(getApplicationContext(), "Listerning..", Toast.LENGTH_SHORT).show();
+
+        Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        speechRecognizer.startListening(speechRecognizerIntent);
     }
 
     private void saveToDB() {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                EditText editText = findViewById(R.id.editTextTextMultiLine);
-                editText.getText();
+
                 ContentValues cv = new ContentValues();
                 cv.put(Story.STORY, editText.getText().toString());
                 getContentResolver().insert(VocaProvider.URI, cv);
@@ -97,8 +193,24 @@ public class NewStory extends AppCompatActivity implements View.OnClickListener 
         linLayout.addView(item);
     }
 
-    @Override
-    public void onClick(View v) {
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, RecordAudioRequestCode);
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RecordAudioRequestCode && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        speechRecognizer.destroy();
     }
 }
--- app/src/main/java/sashanikitin/voca/ScrollingStories.java
+++ app/src/main/java/sashanikitin/voca/ScrollingStories.java
@@ -5,18 +5,32 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.speech.tts.TextToSpeech;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class ScrollingStories extends AppCompatActivity {
+    TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.US);
+                }
+            }
+        });
         buildUI();
     }
 
@@ -42,10 +56,23 @@ public class ScrollingStories extends AppCompatActivity {
 
             item.getLayoutParams().width = LinearLayout.LayoutParams.MATCH_PARENT;
             story.setTextColor(NewStory.colors[color ? 0 : 1]);
+            item.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    read(v);
+                }
+            });
             color = !color;
             linLayout.addView(item);
 
         }
     }
 
+    private void read(View v) {
+        ViewGroup view = (ViewGroup) v;
+        view.getChildCount();
+        TextView textView = (TextView) view.getChildAt(1);
+        Toast.makeText(getApplicationContext(), "hi " + textView.getText(), Toast.LENGTH_SHORT).show();
+        textToSpeech.speak(textView.getText().toString(), TextToSpeech.QUEUE_FLUSH, null,textView.getText().toString());
+    }
 }
